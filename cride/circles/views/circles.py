f7b6d488171ad474a32024e3f9468468422ea3f4
DiazRock <corolariodiaz@gmail.com>
2021-04-17 17:21:19 -0400

    Used genericviews

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,22 +1,32 @@
 """ Circle views """
 
 # Django REST Framework
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
+
+# Permissions
+from cride.permissions.circles import IsCircleAdmin 
 
 
-class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+    ):
     """ Circle viewset """
 
     queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
-    permission_classes = (IsAuthenticated, )
+    lookup_field = 'slug_name'
+    
     
     def get_queryset(self):
         """ Restrict list to public-only """
@@ -24,3 +34,24 @@ class CircleViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return queryset.filter(is_public = True)
         return queryset
+
+    def get_permissions(self):
+        """ Assign permission based on action """
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [p() for p in permissions]
+
+    def perform_create(self, serializer):
+        """ Assign circle admin """
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user = user,
+            profile = profile,
+            circle= circle,
+            is_admin= True,
+            remaining_invitations= 10
+        )
+    
